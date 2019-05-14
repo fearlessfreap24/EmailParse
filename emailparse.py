06c692c2efca34a68f25b3b756565a1f0d01f0d6
fearlessfreap24 <dylanaperez@yahoo.com>
2019-05-13 22:35:53 -0500

    added print statements

--- emailparse.py
+++ emailparse.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
     parse.add_argument('-f', '--file', help='File to be read', required=True)
     args = vars(parse.parse_args())
 
+    print("before if")
     if args['file'] == "" or args['file'] is None:
         print("Input file not detected\n")
         exit(1)
     else:
+        print("in else")
         with open(args['file'], 'rb') as fp:
             headers = BytesParser(policy=default).parse(fp)
 
@@ -20,3 +22,4 @@ if __name__ == "__main__":
             print("{}: {}".format(part, headers[part]))
             # if part == "Received":
             #     print("{}: {}".format(part, headers[part]))
+        print("finished else")
